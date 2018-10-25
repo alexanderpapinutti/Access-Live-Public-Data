2a686c3ff78b790fdd1194a837aff9d9c03a5089
AceVentura <alexpapinutti@gmail.com>
2018-10-25 12:02:12 +0200

    Data Tables Working With Vue.js and fetch()

--- my-vue.js
+++ my-vue.js
@@ -2,16 +2,25 @@ var app = new Vue({
     el: "#app",
     data: {
         members: [],
-        show: true,
+        seen: true,
+        allMembers: [],
+        showFullTable: true,
+        showPartyTable: false,
+        showStatesTable: false,
         partyArray: [],
+        statesArray: [],
+        newStatesArray: [],
         senate: "https://api.propublica.org/congress/v1/113/senate/members.json",
         house: "https://api.propublica.org/congress/v1/113/house/members.json",
     },
     created() {
         if (location.pathname == "/house-data.html") {
-            this.dataTable(this.senate);
-        } else if (location.pathname == "/senate-data.html") {
             this.dataTable(this.house);
+
+
+        } else if (location.pathname == "/senate-data.html") {
+            this.dataTable(this.senate);
+
         }
     },
     methods: {
@@ -28,76 +37,62 @@ var app = new Vue({
                 })
                 .then(function (response) {
                     return response.json();
+
                 })
                 .then(function (data) {
                     app.members = data.results["0"].members;
-                    
+                    app.allMembers = app.members;
+                    app.filterTableByState();
+                    app.showPage();
+                    app.myAlert();
                     
                 })
                 .catch(error => alert(error));
         },
-        updateStateDropdown: function () {
-            var myArray = filterStates();
-            var newArray = [];
-            var select = document.getElementById("state-filter");
-            for (var i = 0; i < myArray.length; i++) {
-                var option = document.createElement("option");
-                var txt = document.createTextNode(myArray[i]);
-                option.appendChild(txt);
-                select.appendChild(option);
-            }
-        },
         selectedState: function () {
             var allRows;
-            allRows = this.members;
-            var stateSelected = document.getElementById("state-filter").value;
-            var statesArray = [];
-            for (var i = 0; i < allRows.length; i++) {
-                if (allRows[i].state == stateSelected) {
-                    statesArray.push(allRows[i]);
-                }
-
-            }
-            return statesArray;
-        },
-        filterTableByParty: function () {
-            var allRows;
+            app.showPage();
+            app.myAlert();
             allRows = app.members;
-
-            app.partyArray = [];
-
+            var stateSelected = document.getElementById("state-filter");
+            app.newStatesArray = [];
             for (var i = 0; i < allRows.length; i++) {
-                if (allRows[i].party == "R" && document.getElementById("republican").checked) {
-                    app.partyArray.push(allRows[i]);
-                } else if (allRows[i].party == "D" && document.getElementById("democrat").checked) {
-                    app.partyArray.push(allRows[i]);
-                } else if (allRows[i].party == "I" && document.getElementById("independent").checked) {
-                    app.partyArray.push(allRows[i]);
+                if (allRows[i].state == stateSelected.value || stateSelected.value == 'all') {
+                    if (allRows[i].party == "R" && document.getElementById("republican").checked) {
+                        app.newStatesArray.push(allRows[i]);
+                    } else if (allRows[i].party == "D" && document.getElementById("democrat").checked) {
+                        app.newStatesArray.push(allRows[i]);
+                    } else if (allRows[i].party == "I" && document.getElementById("independent").checked) {
+                        app.newStatesArray.push(allRows[i]);
+                    } 
                 }
-            }
-            app.partyArray;
-            if (app.partyArray.length > 0){
-                app.show = !app.show;    
-            }else if (app.partyArray=true){
-                app.show = true;
-            }else {
-            }
-            
-        },
+            } 
 
-        filterStates: function () {
-            var allRows = this.members;
+            
+            app.showStatesTable = true;
+            app.showFullTable = false;
+            app.showPartyTable = false;
+        },
+        showPage: function () {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("myDiv").style.display = "block";
+        },
+        filterTableByState: function () {
+            var allRows = app.allMembers;
             var myArray = [];
             for (var i = 0; i < allRows.length; i++) {
                 myArray.push(allRows[i].state);
-
-                app.states = myArray.filter((element, index) => (myArray.indexOf(element) == index));
+                app.statesArray = myArray.filter((element, index) => (myArray.indexOf(element) == index));
             }
-
-            return app.states.sort();
-        }
-
-
+            app.statesArray.sort()
+        },
+        myAlert: function(){
+            if (document.getElementById("republican").checked==false && document.getElementById("democrat").checked==false && document.getElementById("independent").checked==false){
+                app.seen=true;
+            }else {
+                app.seen=false;
+            }
+        },
     }
 });
 
